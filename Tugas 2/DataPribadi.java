7061afb9831e935bb7549d8ae52003ceefe41970
Pikerrr <dedefikri651@gmail.com>
2025-03-05 10:35:31 +0700

    Remove submodules

---
+++ Tugas 2/DataPribadi.java
@@ -0,0 +1,24 @@
+public class DataPribadi {
+    public static void main(String[] args) {
+        // Deklarasi variabel
+        String nim = "12345678";
+        String nama = "Dede Nur Fikri";
+        String alamat = "Jl. gajah mada No. 1";
+        String kota = "Yogyakarta";
+        String kodePos = "56123";
+        String telp = "012345";
+        String hp = "08123456789";
+        String email = "Fikri123@gmail.com";
+
+        // Menampilkan data pribadi
+        System.out.println("Data Pribadi:");
+        System.out.println("NIM      : " + nim);
+        System.out.println("Nama     : " + nama);
+        System.out.println("Alamat   : " + alamat);
+        System.out.println("Kota     : " + kota);
+        System.out.println("Kode Pos : " + kodePos);
+        System.out.println("Telp     : " + telp);
+        System.out.println("HP       : " + hp);
+        System.out.println("Email    : " + email);
+    }
+}
